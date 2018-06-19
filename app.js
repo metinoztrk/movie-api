4a0ee7cfa284ef340afd46e2849deb96a0777cb4
metinoztrk <m.ozturk721@gmail.com>
2018-06-20 01:18:17 +0300

    jwt verify

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const director = require('./routes/director');
 const app = express();
 
 const db=require('./helper/db.js')();
+const config=require('./config');
+app.set('api_secret_key',config.api_secret_key);
+
+const verifyToken=require('./middleware/verify-token')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api',verifyToken);
 app.use('/api/movies', movie);
 app.use('/api/directors', director);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,67 @@
 const express = require('express');
+const bcrypt=require('bcrypt');
+const jvt=require('jsonwebtoken');
 const router = express.Router();
 
+const User=require('../models/Users')
 /* GET home page. */
 router.get('/', (req, res, next)=> {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/register', (req, res, next)=> {
+  const {username,password}=req.body;
+
+  bcrypt.hash(password,10).then((hash)=>{
+    const user=new User({
+      username,
+      password:hash
+    })
+  
+    const promise=user.save();
+  
+    promise.then((data)=>{
+      res.json(data);
+    }).catch((err)=>{
+      res.json(err);
+    });
+  });
+});
+
+router.post('/authenticate',(req,res)=>{
+
+  const {username,password}=req.body;
+
+  User.findOne({username},(err,user)=>{
+    if(err)
+      throw err;
+
+    if(!user){
+      res.json({
+        status:false,
+        message:'Authentication failed, user not found.'
+      });
+    }else{
+      bcrypt.compare(password,user.password).then((result)=>{
+        if(!result){
+          res.json({
+            status:false,
+            message:'Authentication failed, wrong password.'
+          });
+        }else{
+          const payload={
+            username
+          };
+          const token=jvt.sign(payload,req.app.get('api_secret_key'),{expiresIn:720});
+          res.json({
+            status:true,
+            token
+          });
+        }
+      });
+    }
+  });
+
 });
 
 module.exports = router;
